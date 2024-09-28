4762ef26f76daec7ca834ffd1fb4be38b563307c
Srijan Pratap Srivastava <85446215+SRIJANPRATAPSRIVASTAVA@users.noreply.github.com>
2024-09-28 22:21:12 +0530

    callback | added user role | signIn(callback)

--- auth.ts
+++ auth.ts
@@ -2,8 +2,44 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db"
+import { getUserById } from "./data/user"
+import { UserRole } from "@prisma/client"
 
 export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
+    callbacks: {
+        // async signIn({ user }) {
+        //     const existingUser = await getUserById(user.id as string)
+        //     if (!existingUser || !existingUser.emailVerified) {
+        //         return false;
+        //     }
+        //     return true;
+        // },
+        async session({ token, session }) {
+            console.log({
+                sessionToken: token
+            });
+
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            } // https://authjs.dev/getting-started/typescript
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) {
+                return token
+            }
+            const existingUser = await getUserById(token.sub)
+            if (!existingUser) {
+                return token
+            }
+            token.role = existingUser?.role
+            return token
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig
---
+++ next-auth.d.ts
@@ -0,0 +1,22 @@
+import { UserRole } from "@prisma/client";
+import { DefaultSession } from "next-auth";
+
+export declare module "next-auth" {
+    /**
+     * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            /** The user's postal address. */
+            // role: "ADMIN" | "USER";
+            id: string,
+            role: UserRole
+            /**
+             * By default, TypeScript merges new interface properties and overwrites existing ones.
+             * In this case, the default session user properties will be overwritten,
+             * with the new ones defined above. To keep the default session user properties,
+             * you need to add them back into the newly declared interface.
+             */
+        } & DefaultSession["user"]
+    }
+}
