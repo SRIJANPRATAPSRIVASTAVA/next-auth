cf54e11204aec64b6ea2d34c76b22c3e654dd323
Srijan Pratap Srivastava <85446215+SRIJANPRATAPSRIVASTAVA@users.noreply.github.com>
2024-09-28 13:05:16 +0530

    implemented the edge compatibility

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 // /https:/ / authjs.dev / guides / edge - compatibility#middleware
 
-import { auth } from "@/auth"
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
     const isLoggedIn = !!req.auth
